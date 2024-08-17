62958a9de00a9bd8c9f23e025a5a33cdac106819
Robin <1121080+rYuuk@users.noreply.github.com>
2024-08-17 11:27:51 +0200

    chore: clean up

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,8 @@ mod sphere_mesh;
 
 use bevy::diagnostic::LogDiagnosticsPlugin;
 use bevy::input::mouse::MouseMotion;
-use bevy::log::tracing_subscriber::filter::FilterExt;
 use bevy::pbr;
 use bevy::prelude::*;
-use bevy::reflect::ReflectKind::Array;
-use bevy::render::mesh;
 use bevy::render::mesh::{Indices, MeshVertexAttribute, PrimitiveTopology, VertexAttributeValues};
 use bevy::render::render_asset::RenderAssetUsages;
 use bevy::render::render_resource::encase::private::RuntimeSizedArray;
@@ -14,23 +11,15 @@ use bevy::render::render_resource::ShaderRef;
 use bevy_easy_compute::prelude::{AppComputePlugin, AppComputeWorker, AppComputeWorkerBuilder, AppComputeWorkerPlugin, ComputeShader, ComputeWorker};
 use noise::{NoiseFn, Perlin};
 use rand::Rng;
-use sickle_ui::drag_interaction::Draggable;
 use sickle_ui::prelude::{SetJustifyContentExt, SetMarginExt, SetWidthExt, Slider, SliderConfig, UiColumnExt, UiRowExt, UiSliderExt};
-use sickle_ui::prelude::StylableAttribute::Height;
 use sickle_ui::SickleUiPlugin;
 use sickle_ui::ui_builder::{UiBuilderExt, UiRoot};
 use sickle_ui::widgets::inputs::slider::SliderBar;
 use sphere_mesh::SphereMesh;
-// use crate::sphere_mesh::VertexData;
-
-#[derive(Event)]
-struct RandomizeAsteroidEvent;
 
 #[derive(Component)]
 struct Asteroid;
 
-#[derive(Component)]
-struct RandomizeButton;
 #[derive(Resource)]
 struct AsteroidTextures {
     color: Handle<Image>,
@@ -57,12 +46,8 @@ struct SimpleComputeWorker;
 impl ComputeWorker for SimpleComputeWorker {
     fn build(world: &mut World) -> AppComputeWorker<Self> {
         let sphere_mesh = SphereMesh::new(120);
-
         let len = sphere_mesh.vertices.len();
 
-        println!("Build {}", len);
-
-        // Resolution of 4, for example
         let worker = AppComputeWorkerBuilder::new(world)
             .add_staging("vertices", &(sphere_mesh.vertices))
             .add_staging("heights", &vec![0.0; len])
@@ -80,15 +65,11 @@ impl ComputeWorker for SimpleComputeWorker {
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(LogDiagnosticsPlugin::default())
         .add_plugins(SickleUiPlugin)
         .add_plugins(AppComputePlugin)
         .add_plugins(AppComputeWorkerPlugin::<SimpleComputeWorker>::default())
         .insert_resource(Msaa::Sample4)
-        .add_event::<RandomizeAsteroidEvent>()
         .add_systems(Startup, setup)
-        .add_systems(Update, (button_system, randomize_asteroid))
-        // .add_systems(Update, update_mesh_vertices)
         .add_systems(Update, handle_theme_contrast_select)
         .add_systems(Update, rotate_asteroid_system)
         .add_systems(Update, test)
@@ -97,12 +78,9 @@ fn main() {
 
 fn setup(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    mut images: ResMut<Assets<Image>>,
     sphere_mesh: Res<SphereMesh>,
-    // mut compute_worker: ResMut<AppComputeWorker<SimpleComputeWorker>>,
 ) {
     // Camera
     commands.spawn(Camera3dBundle {
@@ -116,16 +94,6 @@ fn setup(
         ..default()
     }, );
 
-    // commands.spawn(PointLightBundle {
-    //     point_light: PointLight {
-    //         intensity: 3000.0,
-    //         shadows_enabled: true,
-    //         ..default()
-    //     },
-    //     transform: Transform::from_xyz(0.0, 8.0, 0.0),
-    //     ..default()
-    // });
-
     commands.spawn(DirectionalLightBundle {
         transform: Transform::from_xyz(0.0, 10.0, 30.0).looking_at(Vec3::ZERO, Vec3::ZERO),
         directional_light: DirectionalLight {
@@ -134,85 +102,6 @@ fn setup(
         },
         ..default()
     });
-
-    // commands.spawn(DirectionalLight::default());
-
-    let textures = AsteroidTextures {
-        color: asset_server.load("textures/asteroid_color.png"),
-        normal: asset_server.load("textures/asteroid_normal.png"),
-        ao: asset_server.load("textures/asteroid_ao.png"),
-        roughness: asset_server.load("textures/asteroid_roughness.png"),
-    };
-    // Add textures as a resource
-    commands.insert_resource(textures);
-    // Asteroid
-    // let world = commands.world_scope(|world| {
-    //     if let Some(textures) = world.get_resource::<AsteroidTextures>() {
-    //         // do_something_with_textures(textures);
-    //         generate(&mut commands, &mut meshes, &mut materials, textures);
-    //     }
-    // });
-
-    // generate(&mut commands, &mut meshes, &mut materials, &textures);
-    // let compute_shader_handle = asset_server.load("shaders/compute_asteroid_height.wgsl");
-
-    // let sphere_mesh = SphereMesh::new(120); // Resolution of 4, for example
-    // let mut new_vertices: Vec<Vec3> = vec![];
-    // for vertex in &sphere_mesh.vertices {
-    //     // let new_vertex = Vec3::new(vertex.x, 1.0 + (vertex.y * 0.2).sin(), vertex.z);
-    //     let new_vertex = vertex.clone();
-    //     new_vertices.push(new_vertex);
-    // }
-    // let mesh = Mesh::from(sphere_mesh);
-
-    // let mut mesh: Mesh = Mesh::new(PrimitiveTopology::TriangleList, RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD);
-    // mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, sphere_mesh.vertices);
-    // mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, sphere_mesh.normals);
-    // mesh.insert_indices(Indices::U32(sphere_mesh.indices));
-
-    // let len = sphere_mesh.vertices.len();
-
-    // compute_worker.write_slice("vertices",&sphere_mesh.vertices.positions);
-    // let heights = vec![0.0f32; len];
-    // compute_worker.write_slice("height", &heights );
-    // compute_worker.write_slice("numVertices",&[len as u32]);
-
-    // let worker: AppComputeWorker<SimpleComputeWorker> = AppComputeWorkerBuilder::new()
-    //     .add_storage("vertices", &sphere_mesh.vertices.positions)
-    //     .add_staging("heights", &vec![0.0; len])
-    //     .add_uniform("numVertices",  &(len as u32))
-    //     .add_uniform("testValue", &3.)
-    //     .add_pass::<SimpleShader>([4, 1, 1], &["vertices", "heights","numVertices","testValue"])
-    //     .one_shot()
-    //     .build();
-    // 
-    // commands.insert_resource(worker);
-
-    println!("Setup");
-
-    let mut mesh: Mesh = Mesh::new(PrimitiveTopology::TriangleList, RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD);
-    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, sphere_mesh.vertices.clone());
-    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, sphere_mesh.normals.clone());
-    mesh.insert_indices(Indices::U32(sphere_mesh.indices.clone()));
-
-    // let mesh = Mesh::from(sphere_mesh);
-
-    // let mesh: Mesh = Mesh::from(sphere_mesh);
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(mesh),
-            material: materials.add(StandardMaterial {
-                base_color: Color::WHITE,
-                perceptual_roughness: 0.5,
-                metallic: 0.2,
-                ..default()
-            }),
-            transform: Transform::from_xyz(0.0, 1.0, 0.0),
-            ..default()
-        },
-        Asteroid
-    ));
-    // Convert SphereMesh into a Bevy Mesh and add it to the mesh assets
 
     commands.spawn(PbrBundle {
         mesh: meshes.add(Circle::new(4.0)),
@@ -239,6 +128,14 @@ fn setup(
             .justify_content(JustifyContent::Center)
             .width(Val::Percent(30.));
     });
+
+    let mut mesh: Mesh = Mesh::new(PrimitiveTopology::TriangleList, RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, sphere_mesh.vertices.clone());
+    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, sphere_mesh.normals.clone());
+    mesh.insert_indices(Indices::U32(sphere_mesh.indices.clone()));
+
+    // let mesh = Mesh::from(sphere_mesh);
+    generate_mesh(commands, meshes, materials, mesh);
 }
 
 fn test(mut compute_worker:
@@ -246,16 +143,14 @@ fn test(mut compute_worker:
         asteroid_query: Query<Entity, With<Asteroid>>,
         mut commands: Commands,
         sphere_mesh: Res<SphereMesh>,
-        mut meshes: ResMut<Assets<Mesh>>,
-        mut materials: ResMut<Assets<StandardMaterial>>,
+        meshes: ResMut<Assets<Mesh>>,
+        materials: ResMut<Assets<StandardMaterial>>,
 ) {
     if !compute_worker.ready() {
         return;
     };
-    
+
     let result: Vec<f32> = compute_worker.read_vec("heights");
-    let result2: Vec<Vec3> = compute_worker.read_vec("vertices");
-    println!("{} {}", result[0], result.len());
 
     if let Ok(asteroid_entity) = asteroid_query.get_single() {
         commands.entity(asteroid_entity).despawn();
@@ -265,10 +160,6 @@ fn test(mut compute_worker:
 
     let mut new_vertices: Vec<Vec3> = vec![];
     for i in 0..vertices.len() {
-        // if result[i] != 1.0
-        // {
-            // println!("point {} {} {}", i, result[i], result2[i]);
-        // }
         new_vertices.push(vertices[i] * result[i]);
     }
 
@@ -276,7 +167,15 @@ fn test(mut compute_worker:
     mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, new_vertices);
     mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, sphere_mesh.normals.clone());
     mesh.insert_indices(Indices::U32(sphere_mesh.indices.clone()));
+    generate_mesh(commands, meshes, materials, mesh);
+}
 
+fn generate_mesh(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mesh: Mesh
+) {
     commands.spawn((
         PbrBundle {
             mesh: meshes.add(mesh),
@@ -291,11 +190,6 @@ fn test(mut compute_worker:
         },
         Asteroid
     ));
-
-    // compute_worker.write_slice::<f32>("heights", &[2., 3., 4., 5.]);
-    // for re in result {
-    // println!("got {:?}", result[0]);
-    // }
 }
 
 fn handle_theme_contrast_select(
@@ -303,30 +197,10 @@ fn handle_theme_contrast_select(
     mut compute_worker: ResMut<AppComputeWorker<SimpleComputeWorker>>,
 ) {
     for mut sliderBar in query.iter_mut() {
-        println!("Called: {}", sliderBar.value());
         compute_worker.write_slice("testValue", &[sliderBar.value()]);
         compute_worker.execute();
     }
 }
-
-// fn update_mesh_vertices(
-//     mut meshes: ResMut<Assets<Mesh>>,
-//     mut query: Query<(&Handle<Mesh>, &mut VertexData), With<Asteroid>>,
-//     time: Res<Time>,
-// ) {
-//     for (mesh_handle, mut vertex_data) in query.iter_mut() {
-//         if let Some(mesh) = meshes.get_mut(mesh_handle) {
-//             if let Some(VertexAttributeValues::Float32x3(positions)) = mesh.attribute_mut(Mesh::ATTRIBUTE_POSITION) {
-//                 // Update vertex positions
-//                 for (i, position) in positions.iter_mut().enumerate() {
-//                     let t = time.elapsed_seconds();
-//                     vertex_data.positions[i].y += (t * 2.0 + i as f32).sin() * 0.01;
-//                     *position = vertex_data.positions[i].to_array();
-//                 }
-//             }
-//         }
-//     }
-// }
 
 fn gizmo_update(mut gizmos: Gizmos, sphere_mesh: Res<SphereMesh>) {
     let center = Vec3::new(0.0, 0.0, 0.0);
@@ -343,128 +217,8 @@ fn gizmo_update(mut gizmos: Gizmos, sphere_mesh: Res<SphereMesh>) {
         gizmos.line(v2, v3, color);
         gizmos.line(v3, v1, color);
     }
-
-    // for vertex in sphere_mesh.vertices.clone() {
-    //     gizmos.sphere(vertex,Quat::default(),0.01,color);
-    // }
 }
 
-fn create_random_asteroid_mesh(size_range: std::ops::Range<f32>) -> Mesh {
-    let mut rng = rand::thread_rng();
-    let perlin = Perlin::default();
-    let size = rng.gen_range(size_range); // Random size within the given range
-
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList, RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD);
-    let mut positions = Vec::new();
-    let mut normals = Vec::new();
-    let mut indices = Vec::new();
-    let mut uvs = Vec::new();
-
-    let sectors = rng.gen_range(50..=100); // Randomize between 10 and 30
-    let stacks = rng.gen_range(60..=100);  // Randomize between 10 and 30
-    let noise_scale = rng.gen_range(0.6..=1.0); // Randomize between 0.1 and 1.0
-    let displacement_factor = rng.gen_range(0.4..=0.6); // Randomize between 0.1 and 1.0
-
-    // let sectors = 12; // Fewer sectors for more irregularity
-    // let stacks = 12; // Fewer stacks for more irregularity
-    // let noise_scale = 1.0; // Larger noise scale for more significant distortion
-    // let displacement_factor = 0.3; // Control the level of bumpiness
-
-    for i in 0..=stacks {
-        let v = i as f32 / stacks as f32;
-        let phi = std::f32::consts::PI * v;
-
-        for j in 0..=sectors {
-            let u = j as f32 / sectors as f32;
-            let theta = std::f32::consts::PI * 2.0 * u;
-
-            // Base spherical coordinates
-            let mut x = phi.sin() * theta.cos();
-            let mut y = phi.cos();
-            let mut z = phi.sin() * theta.sin();
-
-            // Apply Perlin noise to the radius (distance from the origin)
-            let noise_value = perlin.get([x as f64 * noise_scale, y as f64 * noise_scale, z as f64 * noise_scale]) as f32;
-            let displacement = 1.0 + noise_value * displacement_factor;
-            x *= displacement * size;
-            y *= displacement * size;
-            z *= displacement * size;
-
-            positions.push([x, y, z]);
-            normals.push([x, y, z]); // Approximate normals; could be improved
-            uvs.push([u, v]);
-
-            if i < stacks && j < sectors {
-                let k1 = i * (sectors + 1) + j;
-                let k2 = k1 + sectors + 1;
-
-                indices.extend_from_slice(&[k1 as u32, k2 as u32, (k1 + 1) as u32]);
-                indices.extend_from_slice(&[k2 as u32, (k2 + 1) as u32, (k1 + 1) as u32]);
-            }
-        }
-    }
-
-    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
-    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
-    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
-    mesh.insert_indices(Indices::U32(indices));
-
-    mesh
-}
-
-fn button_system(
-    interaction_query: Query<&Interaction, (Changed<Interaction>, With<RandomizeButton>)>,
-    mut randomize_event: EventWriter<RandomizeAsteroidEvent>,
-) {
-    for interaction in interaction_query.iter() {
-        if *interaction == Interaction::Pressed {
-            randomize_event.send(RandomizeAsteroidEvent);
-        }
-    }
-}
-
-fn generate(
-    commands: &mut Commands,
-    meshes: &mut ResMut<Assets<Mesh>>,
-    materials: &mut ResMut<Assets<StandardMaterial>>,
-    textures: &AsteroidTextures,
-) {
-    let material = materials.add(StandardMaterial {
-        base_color_texture: Some(textures.color.clone()),
-        normal_map_texture: Some(textures.normal.clone()),
-        occlusion_texture: Some(textures.ao.clone()),
-        metallic_roughness_texture: Some(textures.roughness.clone()),
-        double_sided: false,
-        ..default()
-    });
-
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(create_random_asteroid_mesh(0.8..2.0)),
-            material,
-            // material: materials.add(Color::WHITE),
-            transform: Transform::from_xyz(0.0, 1.0, 0.0),
-            ..default()
-        },
-        Asteroid,
-    ));
-}
-
-fn randomize_asteroid(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    asteroid_query: Query<Entity, With<Asteroid>>,
-    mut events: EventReader<RandomizeAsteroidEvent>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    textures: Res<AsteroidTextures>,
-) {
-    for _ in events.read() {
-        if let Ok(asteroid_entity) = asteroid_query.get_single() {
-            commands.entity(asteroid_entity).despawn();
-        }
-        generate(&mut commands, &mut meshes, &mut materials, &textures);
-    }
-}
 fn rotate_asteroid_system(
     mut query: Query<&mut Transform, With<Asteroid>>,
     mut mouse_motion_events: EventReader<MouseMotion>,
